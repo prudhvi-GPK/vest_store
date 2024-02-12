527c9090329e5e8843a5c4afb5770372445508d7
sriramteja.ialam@unh.edu <sriramteja.ialam@unh.edu>
2024-02-12 14:04:48 -0500

    Add cart page

--- vest_store/vest_store_app/urls.py
+++ vest_store/vest_store_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-
 from . import views 
 
 urlpatterns = [
    
     path("about/", views.about_page, name="about_page"),
+    path('cart/', views.cart_page, name='cart_page'),
 ]
