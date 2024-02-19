17b42101eb27dec06071710ac095c164b47844e3
Gdeepika56 <dg1201@usnh.edu>
2024-02-19 15:02:48 -0500

    SCRUM-13 add views for home page

--- vest_store/vest_store_app/views.py
+++ vest_store/vest_store_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.from django.shortcuts import render
+def home(request):
+    return render(request, 'vest_store_app/home.html')
 
 def about_page(request):
     return render(request, 'vest_store_app/about.html')
