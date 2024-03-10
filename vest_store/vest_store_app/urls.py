4257337f824e3c9ccd9430c485d5288fa14ace09
Geethanjali-Allam <113144964+Geethanjali-Allam@users.noreply.github.com>
2024-03-09 21:09:16 -0500

    SCRUM-1 create a dropdown list and add item to cart

--- vest_store/vest_store_app/urls.py
+++ vest_store/vest_store_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about_page, name="about_page"),
     path('cart/', views.cart_page, name='cart_page'),
+    path('add_to_cart/<int:quantity>/<str:size>/', views.add_to_cart, name='add_to_cart'),
 ]
