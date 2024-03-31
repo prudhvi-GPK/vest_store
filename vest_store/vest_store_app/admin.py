1dd1c4ba6e4b21edfedb3671aca6c6e0983f655d
prudhvi-GPK <gangavarapuprudhvi@gmail.com>
2024-03-31 12:48:17 -0400

    SCRUM-45 Create superuser and add update home page

--- vest_store/vest_store_app/admin.py
+++ vest_store/vest_store_app/admin.py
@@ -1,3 +1,9 @@
+# admin.py
 from django.contrib import admin
+from .models import Vest
 
-# Register your models here.
+@admin.register(Vest)
+class VestAdmin(admin.ModelAdmin):
+    list_display = ('size', 'price', 'stock')
+    list_editable = ('price', 'stock')
+    list_filter = ('size',)
